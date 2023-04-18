bf89413ae3bb99797b43a7300a47d2e2c258f43e
peterzen <peter@froggle.org>
2023-04-19 00:55:41 +0200

    Remove Snap from Ubuntu 22.04

---
+++ unsnap.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+
+# Based on https://onlinux.systems/guides/20220524_how-to-disable-and-remove-snap-on-ubuntu-2204
+
+echo Disabling snap services
+sudo systemctl disable snapd.service
+sudo systemctl disable snapd.socket
+sudo systemctl disable snapd.seeded.service
+
+echo Removing snaps
+sudo snap remove firefox
+sudo snap remove gnome-3-38-2004
+sudo snap remove gtk-common-themes
+sudo snap remove core20
+sudo snap remove bare
+sudo snap remove snapd
+
+sudo apt autoremove --purge -y snapd
+
+sudo rm -rf /var/cache/snapd/
+rm -rf ~/snap
+
+sudo tee /etc/apt/preferences.d/firefox-no-snap > /dev/null <<EOF
+Package: firefox*
+Pin: release o=Ubuntu*
+Pin-Priority: -1
+
+EOF
+
+
+sudo add-apt-repository -y ppa:mozillateam/ppa
+
+sudo apt install -y firefox
